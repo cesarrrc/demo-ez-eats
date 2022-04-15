ce2328738eb28a47ab181fc4705c0d1a5d6da06a
Sam Dunegan <cesarcisneros9@gmail.com>
2022-04-15 06:55:45 -0500

    fixed hover border, gave insta icon a link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,18 @@ import classes from "./Footer.module.css";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import { Button } from "@mui/material";
 
 export default function Footer() {
   return (
     <footer className={classes.footer_container}>
       <div className={classes.grid_container}>
         <nav>
-          <InstagramIcon className={classes.icon} />
+          <a href="https://www.instagram.com/ezeatstx/">
+            <Button style={{color:'inherit'}}>
+              <InstagramIcon className={classes.icon} />
+            </Button>
+          </a>
           <FacebookIcon className={classes.icon} />
           <AlternateEmailIcon className={classes.icon} />
         </nav>
